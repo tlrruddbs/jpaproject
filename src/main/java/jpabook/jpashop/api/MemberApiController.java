af72e7cfc99cdd10795441ee927c9fac52e554c1
tlrruddbs <tlrruddbs@naver.com>
2021-11-12 10:02:20 +0900

    git  설정

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,5 +1,8 @@
 package jpabook.jpashop.api;
 
+import javax.validation.Valid;
+
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,12 +17,18 @@ public class MemberApiController {
 
 	private final MemberService memberService;
 	
-	public CreateMemberResponse saveMemberV1(Member member) {
+	@PostMapping("/api/v1/members")
+	public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+		Long id = memberService.join(member);
+		return new CreateMemberResponse(id);
 		
 	}
 	
 	@Data
 	static class CreateMemberResponse {
-		private String name;
+		private Long id;
+		public CreateMemberResponse(Long id) {
+			this.id = id;
+		}
 	}
 }
